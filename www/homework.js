00ad9b6336a04c29282cbbdbc5c951f78103495f
Sobgo <60814050+Sobgo@users.noreply.github.com>
2020-08-31 17:09:49 +0200

    Getting started - basic idea

--- www/homework.js
+++ www/homework.js
@@ -1,25 +1,19 @@
 // ------------- Zadania domowe ---------------
 
-function showHomeworkDropdown(event)
-{
-  if(document.getElementById(event.target.id + "Dropdown").style.display == 'none')
-  {
+function showHomeworkDropdown(event){
+  if(document.getElementById(event.target.id + "Dropdown").style.display == 'none'){
     document.getElementById(event.target.id + "Dropdown").style.display = 'flex';
   }
-  else
-  {
+  else{
     document.getElementById(event.target.id + "Dropdown").style.display = 'none';
   }
 }
 
-function showInfo()
-{
-  if(document.getElementById("info").style.display == 'none')
-  {
+function showInfo(){
+  if(document.getElementById("info").style.display == 'none'){
     document.getElementById("info").style.display = 'flex';
   }
-  else
-  {
+  else{
     document.getElementById("info").style.display = 'none';
   }
 }
