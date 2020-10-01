aa3bafd348a9e5fd4b942baa295abd670689e1df
PangolinGiLA <wojtek.j.malecha@gmail.com>
2020-10-01 15:03:39 +0200

    style change and auto load on refresh

--- www/radiowezel/index.php
+++ www/radiowezel/index.php
@@ -13,6 +13,11 @@
 	<input type="checkbox" id="rejected" onchange="refresh()" checked> Odrzucone
 	<input type="checkbox" id="accepted" onchange="refresh()" checked> Zatwierdzone <br>
 	<input type="button" id="refreshbutton" onclick="refresh()" value="odśwież">
+	<script> 
+		$( document ).ready(function() {
+        	refresh()
+		});
+	</script>
 	<div>
 		<ul id="result">		
 		</ul>
